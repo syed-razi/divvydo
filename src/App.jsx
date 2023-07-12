fda546d6a9dd40f8480a894589b192b5d71ed15c
Razi <syedru@mcmaster.ca>
2023-07-11 21:11:47 -0700

    create app.css to modify date-picker css

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import DatePicker from "react-datepicker";
 
-import "react-datepicker/dist/react-datepicker.css";
+import "/src/App.css";
 
 function App() {
   const [startDate, setStartDate] = useState(new Date());
