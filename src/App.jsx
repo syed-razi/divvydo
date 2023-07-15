03fc554c70e37e518c6709e2b13cc8dba43115b4
Razi <syedru@mcmaster.ca>
2023-07-14 19:37:18 -0700

    add breakdown generation

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,8 @@ function App() {
         id: currentDate.getTime(),
         date: new Date(currentDate),
         hours: 0,
+        todo: [],
+        estimate: 0,
       });
       currentDate.setDate(currentDate.getDate() + 1);
     }
@@ -34,6 +36,80 @@ function App() {
   const [questions, setQuestions] = useState([]);
   const [questionNumber, setQuestionNumber] = useState(1);
   const [marks, setMarks] = useState(0);
+  const [generated, setGenerated] = useState(false);
+
+  const totalMarks = questions.reduce(
+    (total, question) => total + question.marks,
+    0
+  );
+  const totalAvailability = availability.reduce(
+    (total, date) => total + date.hours,
+    0
+  );
+
+  function getBreakdown() {
+    const newAvailability = availability.map((a) => ({
+      ...a,
+      todo: [],
+    }));
+
+    let q_pointer = 0;
+
+    let leftOver = 0;
+
+    for (const a of newAvailability) {
+      let marksAvailable = (a.hours / totalAvailability || 0) * totalMarks;
+
+      while (q_pointer < questions.length) {
+        let marksOfQuestion =
+          leftOver != 0 ? leftOver : questions[q_pointer].marks;
+        if (marksAvailable >= marksOfQuestion) {
+          marksAvailable -= marksOfQuestion;
+          a.todo.push(`complete question ${questions[q_pointer].number}`);
+          q_pointer++;
+          leftOver = 0;
+        } else {
+          leftOver = marksOfQuestion - marksAvailable;
+          if (Math.round(marksAvailable) > 0) {
+            a.todo.push(
+              `finish ${Math.round(marksAvailable)} / ${
+                questions[q_pointer].marks
+              } marks of question ${questions[q_pointer].number}`
+            );
+          } else if (marksAvailable === 0) {
+            a.todo.push("take a break :)")
+          }
+          break;
+        }
+      }
+    }
+
+    setAvailability(newAvailability);
+    setGenerated(true);
+  }
+
+  const Breakdown = () => {
+    return (
+      <>
+        {availability.map((a) => (
+          <tr key={a.id}>
+            <td className="border px-10">{a.date.toDateString()}</td>
+            <td className="border px-10">
+              <ul className="list-disc">
+                {a.todo.map((item) => (
+                  <li key={crypto.randomUUID()}>{item}</li>
+                ))}
+              </ul>
+            </td>
+            <td className="border px-10">
+              {Math.round((a.hours / totalAvailability || 0) * estimatedHours)}{" "}
+              hours
+            </td>
+          </tr>
+        ))}
+      </>
+    );
+  };
 
   function handleAddQuestion(e) {
     e.preventDefault();
@@ -61,6 +137,8 @@ function App() {
         id: currentDate.getTime(),
         date: new Date(currentDate),
         hours: 0,
+        todo: [],
+        estimate: 0,
       });
       currentDate.setDate(currentDate.getDate() + 1);
     }
@@ -248,24 +326,23 @@ function App() {
         </div>
         <div className="h-screen w-screen snap-start flex flex-col justify-start items-center space-y-10">
           <h2 className="text-2xl">Breakdown</h2>
-          <div>
-            <table className="inline-block">
-              <thead>
-                <tr>
-                  <th className="border">Day</th>
-                  <th className="border">Questions</th>
-                  <th className="border">Estimate</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td className="border"></td>
-                  <td className="border"></td>
-                  <td className="border"></td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
+          <button className="border p-2" onClick={getBreakdown}>Generate Breakdown</button>
+          {generated && (
+            <div>
+              <table className="inline-block">
+                <thead>
+                  <tr>
+                    <th className="border">Day</th>
+                    <th className="border">To Do</th>
+                    <th className="border">Estimate</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <Breakdown />
+                </tbody>
+              </table>
+            </div>
+          )}
         </div>
       </div>
     </>
