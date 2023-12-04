f006bd833d4a1498ae0bfb08bc62213c60458777
Razi <syedru@mcmaster.ca>
2023-12-04 10:31:47 -0800

    add application shell from tailwindui

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,262 @@
-import { useState } from "react";
+import { Fragment, useState } from "react";
+import { Dialog, Transition } from "@headlessui/react";
 import { QuestionType, AvailabilityType } from "./Types";
 import Dates from "./Dates";
 import Questions from "./Questions";
 import Availability from "./Availability";
 import Breakdown from "./Breakdown";
-
+import {
+  Bars3Icon,
+  DocumentDuplicateIcon,
+  HomeIcon,
+  XMarkIcon,
+  DocumentPlusIcon,
+} from "@heroicons/react/24/outline";
 import "/src/App.css";
 
-function App() {
+type navigationItemType = {
+  name: string;
+  href: string;
+  icon: any;
+  current: boolean;
+};
+
+const initialNavigation: navigationItemType[] = [
+  { name: "Dashboard", href: "#", icon: HomeIcon, current: false },
+  {
+    name: "Assignments",
+    href: "#",
+    icon: DocumentDuplicateIcon,
+    current: true,
+  },
+  {
+    name: "Add Assignment",
+    href: "#",
+    icon: DocumentPlusIcon,
+    current: false,
+  },
+];
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function App() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [navigation, setNavigation] = useState(initialNavigation);
   const [availability, setAvailability] = useState<AvailabilityType[]>([]);
   const [estimatedHours, setEstimatedHours] = useState("");
   const [questions, setQuestions] = useState<QuestionType[]>([]);
 
+  const currentNavItem = navigation.find((navItem) => navItem.current);
+
+  function handleNavigationClick(item: navigationItemType) {
+    const newNavigation = navigation.map((navItem) => {
+      navItem.current = navItem.name === item.name;
+      return navItem;
+    });
+    setNavigation(newNavigation);
+  }
+
   return (
     <>
-      <div className="h-screen snap-y snap-mandatory overflow-scroll scroll-smooth">
-        <Dates
-          estimatedHours={estimatedHours}
-          setEstimatedHours={setEstimatedHours}
-          setAvailability={setAvailability}
-        />
-        <Questions questions={questions} setQuestions={setQuestions} />
-        <Availability
-          availability={availability}
-          setAvailability={setAvailability}
-        />
-        <Breakdown
-          availability={availability}
-          estimatedHours={estimatedHours}
-          questions={questions}
-        />
+      <div>
+        <Transition.Root show={sidebarOpen} as={Fragment}>
+          <Dialog
+            as="div"
+            className="relative z-50 lg:hidden"
+            onClose={setSidebarOpen}
+          >
+            <Transition.Child
+              as={Fragment}
+              enter="transition-opacity ease-linear duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="transition-opacity ease-linear duration-300"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <div className="fixed inset-0 bg-gray-900/80" />
+            </Transition.Child>
+
+            <div className="fixed inset-0 flex">
+              <Transition.Child
+                as={Fragment}
+                enter="transition ease-in-out duration-300 transform"
+                enterFrom="-translate-x-full"
+                enterTo="translate-x-0"
+                leave="transition ease-in-out duration-300 transform"
+                leaveFrom="translate-x-0"
+                leaveTo="-translate-x-full"
+              >
+                <Dialog.Panel className="relative mr-16 flex w-full max-w-xs flex-1">
+                  <Transition.Child
+                    as={Fragment}
+                    enter="ease-in-out duration-300"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="ease-in-out duration-300"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                  >
+                    <div className="absolute left-full top-0 flex w-16 justify-center pt-5">
+                      <button
+                        type="button"
+                        className="-m-2.5 p-2.5"
+                        onClick={() => setSidebarOpen(false)}
+                      >
+                        <span className="sr-only">Close sidebar</span>
+                        <XMarkIcon
+                          className="h-6 w-6 text-white"
+                          aria-hidden="true"
+                        />
+                      </button>
+                    </div>
+                  </Transition.Child>
+                  {/* Sidebar component, swap this element with another sidebar if you like */}
+                  <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6 pb-2 ring-1 ring-white/10">
+                    <div className="flex h-16 shrink-0 items-center">
+                      <img
+                        className="h-8 w-auto"
+                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                        alt="Your Company"
+                      />
+                    </div>
+                    <nav className="flex flex-1 flex-col">
+                      <ul role="list" className="flex flex-1 flex-col gap-y-7">
+                        <li>
+                          <ul role="list" className="-mx-2 space-y-1">
+                            {navigation.map((item) => (
+                              <li key={item.name}>
+                                <a
+                                  href={item.href}
+                                  className={classNames(
+                                    item.current
+                                      ? "bg-gray-800 text-white"
+                                      : "text-gray-400 hover:bg-gray-800 hover:text-white",
+                                    "group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6",
+                                  )}
+                                  onClick={() => handleNavigationClick(item)}
+                                >
+                                  <item.icon
+                                    className="h-6 w-6 shrink-0"
+                                    aria-hidden="true"
+                                  />
+                                  {item.name}
+                                </a>
+                              </li>
+                            ))}
+                          </ul>
+                        </li>
+                      </ul>
+                    </nav>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </Dialog>
+        </Transition.Root>
+
+        {/* Static sidebar for desktop */}
+        <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
+          {/* Sidebar component, swap this element with another sidebar if you like */}
+          <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6">
+            <div className="flex h-16 shrink-0 items-center">
+              <img
+                className="h-8 w-auto"
+                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                alt="Your Company"
+              />
+            </div>
+            <nav className="flex flex-1 flex-col">
+              <ul role="list" className="flex flex-1 flex-col gap-y-7">
+                <li>
+                  <ul role="list" className="-mx-2 space-y-1">
+                    {navigation.map((item) => (
+                      <li key={item.name}>
+                        <a
+                          href={item.href}
+                          className={classNames(
+                            item.current
+                              ? "bg-gray-800 text-white"
+                              : "text-gray-400 hover:bg-gray-800 hover:text-white",
+                            "group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6",
+                          )}
+                          onClick={() => handleNavigationClick(item)}
+                        >
+                          <item.icon
+                            className="h-6 w-6 shrink-0"
+                            aria-hidden="true"
+                          />
+                          {item.name}
+                        </a>
+                      </li>
+                    ))}
+                  </ul>
+                </li>
+                <li className="-mx-6 mt-auto">
+                  <a
+                    href="#"
+                    className="flex items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-white hover:bg-gray-800"
+                  >
+                    <img
+                      className="h-8 w-8 rounded-full bg-gray-800"
+                      src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                      alt=""
+                    />
+                    <span className="sr-only">Your profile</span>
+                    <span aria-hidden="true">Tom Cook</span>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+
+        <div className="sticky top-0 z-40 flex items-center gap-x-6 bg-gray-900 px-4 py-4 shadow-sm sm:px-6 lg:hidden">
+          <button
+            type="button"
+            className="-m-2.5 p-2.5 text-gray-400 lg:hidden"
+            onClick={() => setSidebarOpen(true)}
+          >
+            <span className="sr-only">Open sidebar</span>
+            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+          </button>
+          <div className="flex-1 text-sm font-semibold leading-6 text-white">
+            {currentNavItem?.name || ""}
+          </div>
+          <a href="#">
+            <span className="sr-only">Your profile</span>
+            <img
+              className="h-8 w-8 rounded-full bg-gray-800"
+              src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+              alt=""
+            />
+          </a>
+        </div>
+
+        <main className="py-10 lg:pl-72">
+          <div className="px-4 sm:px-6 lg:px-8">
+            <div className="h-screen snap-y snap-mandatory overflow-scroll scroll-smooth">
+              <Dates
+                estimatedHours={estimatedHours}
+                setEstimatedHours={setEstimatedHours}
+                setAvailability={setAvailability}
+              />
+              <Questions questions={questions} setQuestions={setQuestions} />
+              <Availability
+                availability={availability}
+                setAvailability={setAvailability}
+              />
+              <Breakdown
+                availability={availability}
+                estimatedHours={estimatedHours}
+                questions={questions}
+              />
+            </div>
+          </div>
+        </main>
       </div>
     </>
   );
 }
-
-export default App;
