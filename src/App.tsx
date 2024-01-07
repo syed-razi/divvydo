a0c02792a3612289bd590d1e2f719c32a5d03643
Razi <syedru@mcmaster.ca>
2024-01-07 01:56:40 -0500

    add delete to assignments

--- src/App.tsx
+++ src/App.tsx
@@ -211,7 +211,10 @@ export default function App() {
         <main className="py-10 lg:pl-72">
           <div className="px-4 sm:px-6 lg:px-8">
             {currentNavItem?.name === "Assignments" && (
-              <Assignments assignments={assignments} />
+              <Assignments
+                assignments={assignments}
+                setAssignments={setAssignments}
+              />
             )}
             {currentNavItem?.name === "Add Assignment" && (
               <AddAssignment
